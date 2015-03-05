a36cf31562729deda1e34d6c323ca987e8392105
Ryland Herrick <ryalnd@gmail.com>
2015-03-04 18:18:03 -0600

    Shuffle sentences before displaying them.

--- decontext.js
+++ decontext.js
@@ -1,4 +1,23 @@
 $(function() {
+  var swapItems = function(array, index, otherIndex) {
+    var tempItem = array[index];
+
+    array[index] = array[otherIndex];
+    array[otherIndex] = tempItem;
+
+    return array;
+  },
+  shuffle = function(array) {
+    var randomRemainingIndex;
+
+    array.forEach(function(_, index) {
+      randomRemainingIndex = Math.floor(Math.random() * index);
+      swapItems(array, index, randomRemainingIndex);
+    });
+
+    return array;
+  };
+
   $('form').on('submit', function(e) {
     e.preventDefault();
 
@@ -6,7 +25,8 @@ $(function() {
         text = $('[name=document]').val();
 
     var sentences = text.split(/\.\s+/),
-        paragraphs = "<p>" + sentences.join(".</p><p>") + "</p>";
+        shuffledSentences = shuffle(sentences),
+        paragraphs = "<p>" + shuffledSentences.join(".</p><p>") + "</p>";
 
     $sentences.html(paragraphs);
   });
